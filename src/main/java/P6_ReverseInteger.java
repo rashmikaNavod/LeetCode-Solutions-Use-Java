8d88475ed2487b9df70c1eb78a8315895dce4e12
rashmikaNavod <drashmikanavod@gmail.com>
2026-05-19 11:46:15 +0530

    Add implementation for reversing a signed 32-bit integer

---
+++ src/main/java/P6_ReverseInteger.java
@@ -0,0 +1,36 @@
+
+/**
+
+ Given a signed 32-bit integer x, return x with its digits reversed.
+ If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+ Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+*/
+
+
+
+public class P6_ReverseInteger {
+
+    public static int reverse(int x){
+
+        int reversed = 0;
+
+        while(x!=0){
+            int pop = x % 10;
+            x /= 10;
+
+            if(reversed > Integer.MAX_VALUE/10) return 0;
+            if(reversed < Integer.MIN_VALUE/10) return 0;
+
+            reversed = reversed * 10 + pop;
+        }
+
+        return reversed;
+    }
+
+    public static void main(String[] args) {
+        int output = reverse(123);
+        System.out.println(output);
+    }
+}
